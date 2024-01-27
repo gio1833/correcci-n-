0633e29d7fa85ba78d56b3a01da8f3ca72b7ce38
gio1833 <giozapatagi@gmail.com>
2024-01-27 11:08:15 -0500

    se corregi el 5 y 6

--- ejercicio 6/palindromo.py
+++ ejercicio 6/palindromo.py
@@ -1,9 +1,9 @@
-def es_palindromo(texto)
+def es_palindromo(texto):# se le agrega los dos puntos al final para que la sintaxis pueda continar y asi no tener errorer
     texto = ''.join(caracter.lower() for caracter in texto if caracter.isalnum())
     return texto == texto[::-1]
 
 palabra_frase = input("Ingrese una palabra o frase: ")
-if es_palindromo():
+if es_palindromo(palabra_frase):# se le agrega a la variable la funcion para el polindromo
     print(f"{palabra_frase} es un palíndromo.")
 else:
     print(f"{palabra_frase} no es un palíndromo.")
