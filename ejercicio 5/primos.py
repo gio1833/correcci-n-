63077a499fe28e029da3b07fd3255a74ff9c5c0c
gio1833 <giozapatagi@gmail.com>
2024-01-27 09:54:06 -0500

    ejercicio 5 corregido

--- ejercicio 5/primos.py
+++ ejercicio 5/primos.py
@@ -1,11 +1,11 @@
-def es_primo(numero)
-    if numero < 2
+def es_primo(numero): # se corregio porque le faltaba los dos puntos 
+    if numero < 2: # se define la variable numero 
         return False
-    for i in range(2, int(numero**0.5) + 1)
-        if numero % i == 0
-            retrun False
-    retunr True
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            return False # se corregi return en la linea estaba mal escrito 
+    return True # se corregi return en la linea estaba mal escrito 
 
 limite = int(input("Ingrese el límite superior para encontrar números primos: "))
-primos = [num for num in range(2, limite + 1) if es_primo()]
+primos = [num for num in range(2, limite + 1) if es_primo(num)]# se agrega num en la funcion
 print("Números primos:", primos)
