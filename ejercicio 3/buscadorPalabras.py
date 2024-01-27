0d01f5b4206b897f18e3901fa74e49ac899e8295
gio1833 <giozapatagi@gmail.com>
2024-01-27 09:19:39 -0500

    ejercicio 3 corregido

--- ejercicio 3/buscadorPalabras.py
+++ ejercicio 3/buscadorPalabras.py
@@ -1,7 +1,7 @@
-def contar_palabra(texto, palabra)
+def contar_palabra(texto, palabra):# se agrega en la definicion los dos puntos
     return texto.lower().split().count(palabra.lower())
 
 texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
 palabra_buscada = "texto"
 
-print(f"La palabra '{palabra_buscada aparece contar_palabra(texto, palabra_buscada)} veces.")
+print(f"La palabra '{palabra_buscada} aparece {contar_palabra(texto, palabra_buscada)} veces.")# se le agregan las llaves en las palabra buscada y en contar palabra
