223a3d153755db329c80678772efd478da4d0109
gio1833 <giozapatagi@gmail.com>
2024-01-27 08:44:24 -0500

    primera subida

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,19 @@
 def calculadora():
-    num1 = float("Ingrese el primer número: ")
-    num2 = float("Ingrese el segundo número: ")
+    num1 = float(input("Ingrese el primer número:"))
+    num2 = float(input("Ingrese el segundo número:"))
     operacion = input("Ingrese la operación (+, -, *, /): ")
 
     if operacion == '+':
-        resultado = num + num2
+        resultado = num1 + num2 # correc 
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num / num2
+        resultado = num1 / num2 # correc
     else:
         resultado = "Operación no válida"
+    
+    print("Resultado: ",resultado) # correc
 
-    print("Resultado: "resultado)
-
-calculdora()
+calculadora()
