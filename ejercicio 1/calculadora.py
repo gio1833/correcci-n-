5074249eaf06b28db3dfbd73cd85c8b1934388e8
gio1833 <giozapatagi@gmail.com>
2024-01-27 09:09:03 -0500

    ejercicios 1 y 2 corregidos

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,19 @@
 def calculadora():
-    num1 = float(input("Ingrese el primer número:"))
-    num2 = float(input("Ingrese el segundo número:"))
+    num1 = float(input("Ingrese el primer número:")) # convertir esa entrada y luego almacenarlo en la variable
+    num2 = float(input("Ingrese el segundo número:"))# convertir esa entrada y luego almacenarlo en la variable
     operacion = input("Ingrese la operación (+, -, *, /): ")
 
     if operacion == '+':
-        resultado = num1 + num2 # correc 
+        resultado = num1 + num2 # se difiene num como varibale num1
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num1 / num2 # correc
+        resultado = num1 / num2 # se difine num como variable num1
     else:
         resultado = "Operación no válida"
     
-    print("Resultado: ",resultado) # correc
+    print("Resultado: ",resultado) # se coloca coma despues de cerrar comillas
 
 calculadora()
--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,9 @@
-import rando
+import random # se modifica random no esta bien llamado en funcion
 import string
 
-generar_contraseña(longitud=8):
+def generar_contraseña(longitud=8): # se llama def para llamar la fincion
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
+    return contraseña
 
-print(generar_contrasena())
+print(generar_contraseña()) # se corregi el palabra contaraseña la n
