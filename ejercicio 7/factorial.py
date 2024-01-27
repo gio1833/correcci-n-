6e98d6f0173fd6815f267723e21eafaf38c94c54
gio1833 <giozapatagi@gmail.com>
2024-01-27 11:38:51 -0500

    ejercicio 7 corregido

--- ejercicio 7/factorial.py
+++ ejercicio 7/factorial.py
@@ -1,8 +1,8 @@
-def factorial(n)
-    if n == 0 or n = 1
-        retunr 1
+def factorial(n):# se corrige y se agregan los dos puntos para que la funcion se ejecute
+    if n == 0 or n == 1: # se agrega un (=) para que sea valida la comparacion y asi poder ejecutar la variable
+        return 1
     else:
-        retunr n * factorial(n - 1)
+        return n * factorial(n - 1)
 
 numero = int(input("Ingrese un número para calcular su factorial: "))
-print(f"El factorial de {numero} es {factorial()}")
+print(f"El factorial de {numero} es {factorial(numero)}") # se agrega la palabra numero para la funcion de la variable
