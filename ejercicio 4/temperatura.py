d25278e2df5d70d27f3047b429cdbd55681a44ac
gio1833 <giozapatagi@gmail.com>
2024-01-27 09:27:10 -0500

    ejercicio 4 corregido

--- ejercicio 4/temperatura.py
+++ ejercicio 4/temperatura.py
@@ -1,6 +1,6 @@
-def celsius_a_fahrenheit(celsius)
-    return (celsius * 9/5)  32
+def celsius_a_fahrenheit(celsius):# se agregan en la def los dos puntos 
+    return (celsius * 9/5)  + 32 # en la linea se le agrega el signo + para que la variable 
 
-temperatura_celsius = float("Ingrese la temperatura en Celsius: ")
+temperatura_celsius = float (input("Ingrese la temperatura en Celsius: ")) # se le agreaga input en la variable
 temperatura_fahrenheit = celsius_a_fahrenheit(temperatura_celsius)
 print(f"{temperatura_celsius}°C es igual a {temperatura_fahrenheit}°F")
