fa326dc8c59389a5b2a3ff4224c0dbac5235b46c
gio1833 <giozapatagi@gmail.com>
2024-02-10 07:51:05 -0500

    Ejercicios 9 y 10

--- ejercicio 10/descuento.py
+++ ejercicio 10/descuento.py
@@ -1,9 +1,9 @@
-calcular_precio_con_decuento(precio_base, porcentaje_descuento)
+def calcular_precio_con_descuento(precio_base, porcentaje_descuento): # se corregi la parabra descuento y se le añade a la funcion def
     descuento = precio_base * (porcentaje_descuento / 100)
     precio_final = precio_base - descuento
-    retunr precio_final
+    return precio_final # se corregi return
 
-precio_base = float("Ingrese el precio base del producto: ")
+precio_base = float (input("Ingrese el precio base del producto: ")) # se corrige el input
 porcentaje_descuento = float(input("Ingrese el porcentaje de descuento: "))
 precio_final = calcular_precio_con_descuento(precio_base, porcentaje_descuento)
 print(f"El precio final con {porcentaje_descuento}% de descuento es: {precio_final}")
--- ejercicio 9/dados.py
+++ ejercicio 9/dados.py
@@ -1,10 +1,10 @@
-import radom
+import random # se corregi la palabra random
 
-simular_lanzamiento_dados(cantidad_dados, caras_por_dado)
+def simular_lanzamiento_dados(cantidad_dados, caras_por_dado):
     resultados = [random.randint(1, caras_por_dado) for _ in range(cantidad_dados)]
-    retunr resultados
+    return resultados # se corregi la palabra return 
 
 cantidad_dados = int(input("Ingrese la cantidad de dados a lanzar: "))
 caras_por_dado = int(input("Ingrese la cantidad de caras por dado: "))
-lanzamientos = simular_lanzamiento_dados(, caras_por_dado)
+lanzamientos = simular_lanzamiento_dados(cantidad_dados, caras_por_dado) # se añade argumento cantidad_datos
 print(f"Resultados del lanzamiento: {lanzamientos}")
